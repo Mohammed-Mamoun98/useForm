2421b7be48adb6b7419fe2761e5bfb83f3b67a17
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2021-09-13 11:42:45 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { useForm } from './From.js';
 import { fields } from './fields.js';
+import * as yup from 'yup';
 import './style.css';
+const validationSchema = yup.object().shape({
+  name: yup
+    .string('')
+    .min(12, 'min is 12 chars')
+    .required('Field is required'),
+  age: yup.number().required('Field is required'),
+  country: yup.string().required('Field is required')
+});
 
 export default function App() {
-  const [Render, formik] = useForm({ fields });
+  const [render, formik] = useForm({ fields, validationSchema });
   return (
     <div>
       {JSON.stringify(formik.values)}
-      <Render />
+      {render(<button type="submit">test</button>)}
     </div>
   );
 }
--- src/From.js
+++ src/From.js
@@ -1,34 +1,59 @@
 import React, { useCallback } from 'react';
 import { useFormik } from 'formik';
+import * as yup from 'yup';
 
-export const useForm = ({ fields = [], initialValues = {}, ...rest }) => {
+export const useForm = ({
+  fields = [],
+  initialValues = {},
+  onSubmit = () => {},
+  ...rest
+}) => {
+  const handleSubmit = values => {
+    onSubmit(values);
+  };
   const formik = useFormik({
     values: initialValues || {},
     initialValues,
+    onSubmit: handleSubmit,
     ...rest
   });
-  const { handleChange, handleBlur, setFieldValue, values } = formik;
-  const Render = useCallback(() => {
-    console.log('render');
-    return (
-      <>
-        {fields.map(field => (
-          <>
-            {field?.render ? (
-              field?.render(formik)
-            ) : (
-              <input
-                {...field}
-                name={field.name}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values?.[field.name]}
-              />
-            )}
-          </>
-        ))}
-      </>
-    );
-  }, []);
-  return [Render, formik];
+  const { handleChange, handleBlur, setFieldValue, values, errors } = formik;
+
+  const render = useCallback(
+    children => {
+      console.log('render');
+      return (
+        <form
+          onSubmit={formik.handleSubmit}
+          style={{ display: 'flex', flexDirection: 'column' }}
+        >
+          {fields.map(field => (
+            <>
+              {field?.render ? (
+                field?.render(formik)
+              ) : (
+                <>
+                  <input
+                    key={field.name}
+                    {...field}
+                    name={field.name}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values?.[field.name]}
+                    style={{
+                      border: errors?.[field.name] ? '1px red solid' : '',
+                      outline: 'none'
+                    }}
+                  />
+                </>
+              )}
+            </>
+          ))}
+          {children}
+        </form>
+      );
+    },
+    [formik.values, formik.errors]
+  );
+  return [render, formik];
 };
